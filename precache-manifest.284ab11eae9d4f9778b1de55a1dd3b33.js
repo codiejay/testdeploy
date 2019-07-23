77ff86b9cd0a4d8f636b806b9fcdf095fbfda539
James Akpan <arfitcode@gmail.com>
2019-07-23 14:25:46 +0100

    Updates

---
+++ precache-manifest.284ab11eae9d4f9778b1de55a1dd3b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a5009a3a6d538febd707da88444244",
+    "url": "/testdeploy/index.html"
+  },
+  {
+    "revision": "d94e8617b545b787fab7",
+    "url": "/testdeploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testdeploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d94e8617b545b787fab7",
+    "url": "/testdeploy/static/js/main.81c1e4f2.chunk.js"
+  },
+  {
+    "revision": "ad6ad792628b989e4a31",
+    "url": "/testdeploy/static/js/runtime~main.f4eb86ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testdeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
